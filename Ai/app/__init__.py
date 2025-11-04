83c40a1fa7cac5acbd9a705cb8efa0285e3bb1d4
Pahari47 <nachiketapahari00@gmail.com>
2025-11-04 20:07:05 +0530

    structure done

---
+++ Ai/app/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+import os
+
+def create_app():
+    app = Flask(__name__)
+    CORS(app)
+
+    # Health
+    @app.get("/")
+    def index():
+        return jsonify({
+            "status": "ok",
+            "service": "Smart Farming solution by Pahari",
+            "endpoints": []
+        })
+    
+
+    return app

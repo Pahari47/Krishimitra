f6ebfc6a0d8f029fec92f8fff70965622336357e
Pahari47 <nachiketapahari00@gmail.com>
2025-11-05 22:48:23 +0530

    weather service

---
+++ Ai/app/services/weather_service.py
@@ -0,0 +1,23 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv("OPENWEATHER_API_KEY")
+
+def get_weather(lat, lon):
+    url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}&units=metric"
+
+    res = requests.get(url)
+
+    if res.status_code != 200:
+        return {"error": "Failed to fetch weather data"}
+    
+    data = res.json()
+
+    return {
+        "temperature": data["main"]["temp"],
+        "humidity": data["main"]["humidity"],
+        "rainfall": data.get("rain", {}).get("1h", 0)
+    }

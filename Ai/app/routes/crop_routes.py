69d904bcaba95dabbd2c05c1554afe8eb125e907
Pahari47 <nachiketapahari00@gmail.com>
2025-11-05 23:03:19 +0530

    routes working

--- Ai/app/routes/crop_routes.py
+++ Ai/app/routes/crop_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify
 from app.services.crop_service import recommend_crop
+from app.services.weather_service import get_weather
 
 crop_bp = Blueprint("crop", __name__, url_prefix="/api")
 
@@ -15,3 +16,29 @@ def crop_recommend_api():
         return jsonify({"status": "success", "data": result})
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)}), 400
+    
+
+
+@crop_bp.post("/recommend/auto")
+def crop_recommended_auto():
+    data = request.get_json()
+    soil = data.get("soil", {})
+    lat = data.get("lat")
+    lon = data.get("lon")
+
+    if lat is None or lon is None:
+        return jsonify({"status": "error", "message": "lat & lon required"}), 400
+    
+    weather = get_weather(lat, lon)
+
+    if "error" in weather:
+        return jsonify({"status": "error", "message": weather["error"]})
+    
+    result = recommend_crop(soil, weather)
+
+    return jsonify({
+        "success": "success",
+        "source": "live_weather",
+        "weather": weather,
+        "data": result
+    })
